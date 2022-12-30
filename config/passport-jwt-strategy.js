d35d40e43e82a4a18393944aa43f90776fd134c3
Nikhil Dixit <nikhil09.dixit@gmail.com>
2022-12-30 19:48:39 +0530

    v3

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -3,9 +3,10 @@ const JWTStrategy=require('passport-jwt').Strategy;
 const ExtractJWT=require('passport-jwt').ExtractJwt;
 const User=require('../models/user');
 const env=require('./environment');
+console.log(env.jwtSecret);
 let opts={
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: env.jwtSecret
+    secretOrKey: "secret"
 };
 //from payloads
 passport.use(new JWTStrategy(opts,function(jwtPayload,done){
